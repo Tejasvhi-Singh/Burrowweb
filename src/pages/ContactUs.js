66451b8c6b5b56ebbc30f92299762bcdc4780125
Tejashvi singh <1032212055@mitwpu.edu.in>
2025-02-17 18:53:45 +0530

    Fix typo in contact name in ContactUs component

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -10,7 +10,7 @@ const ContactUs = () => (
     <h1>Contact Us</h1>
     <p>If you have any questions or need assistance, feel free to reach out to our team below.</p>
     <ContactSection name="Aryan Goyal" email="aryangoyalbusiness@gmail.com" phone="+91 6396667308" imgSrc={man} />
-    <ContactSection name="Tejasvi Singh" email="Tejayashsingh@gmail.com" phone="+91 6394468891" imgSrc={manager} />
+    <ContactSection name="Tejasvhi Singh" email="Tejayashsingh@gmail.com" phone="+91 6394468891" imgSrc={manager} />
     <ContactSection name="Prince Pratap" email="princepratapism@gmail.com" phone="+91 7668629313" imgSrc={boy} />
   </main>
 );
